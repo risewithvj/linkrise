f4163b56b1b18ab12f67ee0a80f8f3d1f2a9db91
Vijaya Kumar L <risewithvj@gmail.com>
2026-03-08 03:46:36 +0530

    Linkrise

--- uninstall.php
+++
@@ -1,4 +0,0 @@
-<?php
-// This file is called by WordPress when the plugin is deleted.
-// The actual cleanup is performed by linkrise_on_uninstall() in linkrise.php.
-if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) { exit; }
